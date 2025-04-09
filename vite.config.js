7251054a313515fd2ccbe94252debeba4e018d89
Shivam-2309 <116469216+Shivam-2309@users.noreply.github.com>
2025-04-09 20:34:49 +0530

    bug-fix1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Adjust this if your app is hosted in a subdirectory
 })
